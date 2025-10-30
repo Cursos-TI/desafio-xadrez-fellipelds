0e96ef4205294911208e6c65203394b2fb725bee
Fellipe Lima <fellipelima199@gmail.com>
2025-10-30 02:11:37 +0000

    nivel mestre ok

--- xadrez.c
+++ xadrez.c
@@ -1,35 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
+void movtorre (int movt) { //movtorre: movimento da torre
+    if (movt > 0) {
+        printf("Direita \n");
+        movtorre(movt - 1); //movt: movimento da torre
+    }
+}
+
+void movrainha (int movr) { //movrainha: movimento da rainha
+    if (movr > 0) {
+        printf("Esquerda \n");
+        movrainha(movr - 1); //movr: movimento da rainha
+    }
+}
+
+void movbispo (int movb) { //movbispo: movimento do bispo
+    if (movb > 0) { //movb: movimento do bispo
+    for (int bv = 0; bv < 1; bv++) {
+        for (int bh = 0; bh < 1; bh++) {
+            printf("Cima \n");
+        }
+    } printf("Direita \n");
+    movbispo(movb - 1);
+    }
+}
 
 int main() {
-    int b, r, t, c;
+    int b, r, t, c; //Variaves - b: bispo - r: rainha - t: torre - c: cavalo
+    t = 5;
     printf("Movimentação da Torre:\n");
-    for( t = 0; t < 5; t++){
-        printf("Direita\n"); //Direção do movimento da Torre
-    }
+    movtorre(t);
 
     printf("\nMovimentação do Bispo:\n");
-    b = 1;
-    while(b <= 5){
-        printf("Cima, direita\n"); //Direção do movimento do Bispo
-        b++;
-    }
+    b = 5;
+    movbispo(b);
 
     printf("\nMovimentação da Rainha:\n");
-    r = 1;
-    do {
-        printf("Esquerda\n"); //Direção do movimento da Rainha
-        r++;
-    } while (r <= 8);
+    r = 8;
+    movrainha(r);
 
     printf("\nMovimentação do Cavalo:\n");
-    for (c = 0; c <= 2; c++) {
-        do {printf("Baixo\n"); //Direção do movimento do Cavalo
+    for (c = 0; c <= 2, c >= 0; c++) {
+        do {printf("Cima\n");
         c++;
         }
         while(c < 2);
-    } printf("Esquerda\n");
+        break;
+    } printf("Direita\n");
 
     return 0;
 }
